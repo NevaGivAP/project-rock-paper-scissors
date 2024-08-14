638205ef3c5bbf1ff13a8fb13a6e0cfee16f2b23
Ibuki Ai <168512406+NevaGivAP@users.noreply.github.com>
2024-08-14 23:13:53 +0700

    Move the gameplay to HTML
    the results are not printed in the console anymore, but printed inside
    divs in the HTML. User input is not taken from the alert anymore, but
    taken through buttons that will trigger event listeners if clicked.

--- script.js
+++ script.js
@@ -10,68 +10,96 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let choice = prompt("0 for rock, 1 for paper, 2 for scissors");
-    switch (choice){
-        case "0":
-            return "rock";
-        case "1":
-            return "paper";
-        case "2":
-            return "scissors";
-    }
-}
+let humanScore = 0;
+let computerScore = 0;
+let draw = 0;
 
 
-
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
+    
 
     function playRound(humanChoice,computerChoice){
+        const message = document.querySelector("#message")
+        const playerSum = document.querySelector("#player-sum");
+        const computerSum = document.querySelector("#computer-sum");
+        const winnerMessage = document.querySelector("#winner-message");
+        const drawSum = document.querySelector("#draw-sum");
+
+        message.textContent = "";
+        winnerMessage.textContent = "";
+        
         if(humanChoice === computerChoice){
-            return "Draw"
-        }
-        if(humanChoice === 'rock'){
+            message.textContent =  "Draw";
+            draw += 1;
+            drawSum.textContent = `Draw: ${draw}`
+        } else if(humanChoice === 'rock'){
             if(computerChoice === 'paper'){
                 computerScore += 1;
-                return "You lose! Paper beats Rock"
+                message.textContent = "You lose! Paper beats Rock";
+                computerSum.textContent = `Computer Score: ${computerScore}`;
             } else{
                 humanScore += 1;
-                return "You win! Rock beats Scissors"
+                message.textContent = "You win! Rock beats Scissors";
+                playerSum.textContent = `Player Score: ${humanScore}`;
             }
         } else if(humanChoice === 'paper'){
             if(computerChoice === 'scissors'){
                 computerScore += 1;
-                return "You lose! Scissors beats Paper"
+                message.textContent = "You lose! Scissors beats Paper";
+                computerSum.textContent = `Computer Score: ${computerScore}`;
             } else{
-            humanScore += 1;
-            return "You win! Paper beats Rock"
+                humanScore += 1;
+                message.textContent = "You win! Paper beats Rock";
+                playerSum.textContent = `Player Score: ${humanScore}`;
             }
         } else{
             if(computerChoice === 'rock'){
                 computerScore += 1;
-                return "You lose! Rock beats Scissors"
+                message.textContent = "You lose! Rock beats Scissors";
+                computerSum.textContent = `Computer Score: ${computerScore}`;
             } else{
                 humanScore += 1;
-                return "You win! Scissors beats Paper"
+                message.textContent = "You win! Scissors beats Paper";
+                playerSum.textContent = `Player Score: ${humanScore}`;
             }
         }
+        console.log(draw)
+        console.log(humanScore)
+        console.log(computerScore)
+        if(draw + humanScore + computerScore === 5){
+            if(humanScore > computerScore){
+                winnerMessage.textContent = `You scored: ${humanScore}, Computer scored ${computerScore}, Congrats, You Win!`;
+            } else if(humanScore < computerScore){
+                winnerMessage.textContent =  `You scored: ${humanScore}, Computer scored ${computerScore}, Sorry, Computer Wins!`;
+            } else{
+                winnerMessage.textContent =  'Is it even possible to get a draw?';
+            }
+            humanScore = 0;
+            computerScore = 0;
+            draw = 0;
+
+            drawSum.textContent = "Draw: 0";
+            computerSum.textContent = "Computer Score: 0";
+            playerSum.textContent = "Player Score: 0";
+        }
+        
     }
 
-    console.log(playRound(getHumanChoice(),getComputerChoice()));
-    console.log(playRound(getHumanChoice(),getComputerChoice()));
-    console.log(playRound(getHumanChoice(),getComputerChoice()));
-    console.log(playRound(getHumanChoice(),getComputerChoice()));
-    console.log(playRound(getHumanChoice(),getComputerChoice()));
+    
 
-    if(humanScore > computerScore){
-        return `You scored: ${humanScore}, Computer scored ${computerScore}, Congrats, You Win!`
-    } else if(humanScore < computerScore){
-        return `You scored: ${humanScore}, Computer scored ${computerScore}, Sorry, Computer Wins!`
-    } else{
-        return 'Is is even possible to get a draw?'
+
+const menu = document.querySelector("#menu");
+menu.addEventListener("click", (item) => {
+    let target = item.target;
+
+    switch(target.id){
+        case 'rock':
+            playRound('rock',getComputerChoice());
+            break;
+        case 'paper':
+            playRound('paper',getComputerChoice());
+            break;
+        case 'scissors':
+            playRound('scissors',getComputerChoice());
+            break;
     }
-}
-
-console.log(playGame());
+});
